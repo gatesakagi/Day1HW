30f151caab55ba523a480b1a1f79ed644b7f40ca
gatesakagi <gatesakagi@gmail.com>
2016-05-05 01:57:48 +0800

    Day 4 - 新增Area，可以依Role來區別。 但非Admin Role帳號，登入後台後無法自動轉到首頁。

---
+++ Day1HW/Areas/Admin/Controllers/EditController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day1HW.Areas.Admin.Controllers
+{
+    [Authorize(Roles ="Admins")]
+    public class EditController : Controller
+    {
+        // GET: Admin/Edit
+        public ActionResult Index()
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+                return View();
+            } else
+            {
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+            
+        }
+    }
+}
